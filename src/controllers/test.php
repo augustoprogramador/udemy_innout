68433fcb5488b87f6d8d83f03c13ea86c58af003
augustoprogramador <augustoprogramador@hotmail.com>
2021-06-30 19:02:12 -0300

    cálculo da hora de almoço

--- src/controllers/test.php
+++ src/controllers/test.php
@@ -8,3 +8,8 @@
 
     $workedIntervalString = $wh->getWorkedInterval()->format('%H:%I:%S');
     print_r($workedIntervalString);
+    
+    echo "<br>";
+
+    $lunchIntervalString = $wh->getLunchInterval()->format('%H:%I:%S');
+    print_r($lunchIntervalString);
