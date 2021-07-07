9b62f59db9e94c0db3489e953717571c32ade779
augustoprogramador <augustoprogramador@hotmail.com>
2021-07-07 15:01:44 -0300

    melhoria da view manager_report

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,5 +1,51 @@
 <main class="content">
-    <?php echo "Usuários Ativos: {$activeUsersCount}<br>" ?>
-    <?php echo "Horas trabalhadas no mês: {$hoursInMonth}<br>" ?>
-    <?php echo '<pre>'; print_r($absentUsers); echo '</pre>'; ?>
+    <?php 
+        renderTitle(
+            'Relatório gerencial',
+            'Resumo das horas trabalhadas dos funcionários',
+            'icofont-chart-histogram'
+        )
+    ?>
+
+    <div class="sumary-boxes">
+        <div class="sumary-box bg-primary">
+            <i class="icofont-users"></i>
+            <p class="title">Qtde de Funcionários</p>
+            <h3 class="value"><?php echo $activeUsersCount ?></h3>
+        </div>
+        <div class="sumary-box bg-danger">
+            <i class="icofont-patient-bed"></i>
+            <p class="title">Faltas</p>
+            <h3 class="value"><?php echo count($absentUsers) ?></h3>
+        </div>
+        <div class="sumary-box bg-success">
+            <i class="icofont-sand-clock"></i>
+            <p class="title">Horas no Mês</p>
+            <h3 class="value"><?php echo $hoursInMonth ?></h3>
+        </div>
+    </div>
+
+    <?php if(count($absentUsers) > 0): ?>
+        <div class="card mt-4">
+            <div class="card-header">
+                <div class="card-title">Faltosos do Dia</div>
+                <p class="card-category">Relação dos funcionários que ainda não bateram ponto.</p>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-hover table-striped">
+                    <thead>
+                        <th>Nome</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach($absentUsers as $name): ?>
+                        <tr>
+                            <td><?php echo $name ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-footer"></div>
+        </div>
+    <?php endif; ?>
 </main>
