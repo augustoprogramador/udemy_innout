bd660deb1a210f46b9f23caa9d4433fe87166a33
augustoprogramador <augustoprogramador@hotmail.com>
2021-07-07 13:38:13 -0300

    relatório gerencial, criação da view

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,3 +1,5 @@
 <main class="content">
-    <h1>OK!</h1>
+    <?php echo "Usuários Ativos: {$activeUsersCount}<br>" ?>
+    <?php echo "Horas trabalhadas no mês: {$hoursInMonth}<br>" ?>
+    <?php echo '<pre>'; print_r($absentUsers); echo '</pre>'; ?>
 </main>
